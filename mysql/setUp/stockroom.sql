85f6c16c17e90f1d17787452c432d57415aa50e3
onesword0618 <ao.akua.leo@gmail.com>
2022-01-10 22:40:13 +0900

    Add:仮想環境上にMySQLを追加した
    databaseの設計を考えるために仮想環境でdatabaseを構築した

---
+++ mysql/setUp/stockroom.sql
@@ -0,0 +1,19 @@
+-- scenario
+DROP TABLE IF EXISTS scenario.stocks;
+CREATE TABLE scenario.stocks (id varchar(50), name varchar(50), address varchar(50));
+INSERT INTO scenario.stocks VALUES('1','1st Stock Place','1st Area');
+INSERT INTO scenario.stocks VALUES('2','2nd Stock Place','2nd Area');
+INSERT INTO scenario.stocks VALUES('3','3rd Stock Place','3st Area');
+INSERT INTO scenario.stocks VALUES('4','4th Stock Place','4st Area');
+INSERT INTO scenario.stocks VALUES('5','5th Stock Place','5st Area');
+INSERT INTO scenario.stocks VALUES('6','6th Stock Place','6st Area');
+INSERT INTO scenario.stocks VALUES('7','7th Stock Place','7st Area');
+INSERT INTO scenario.stocks VALUES('8','8th Stock Place','8st Area');
+INSERT INTO scenario.stocks VALUES('9','9th Stock Place','9st Area');
+INSERT INTO scenario.stocks VALUES('10','10th Stock Place','10st Area');
+INSERT INTO scenario.stocks VALUES('11','11th Stock Place','11st Area');
+INSERT INTO scenario.stocks VALUES('12','12th Stock Place','12st Area');
+INSERT INTO scenario.stocks VALUES('13','13th Stock Place','13st Area');
+INSERT INTO scenario.stocks VALUES('14','14th Stock Place','14st Area');
+INSERT INTO scenario.stocks VALUES('15','15th Stock Place','15st Area');
+SELECT * FROM scenario.stocks;
