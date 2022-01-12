ffe4e1888d61c55284c54734b2c073e4489e9d31
onesword0618 <ao.akua.leo@gmail.com>
2022-01-12 23:34:08 +0900

    Update: pathの内容を更新した

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,14 +5,24 @@
  * Kenichi Inoue.
  */
 import express, { Application } from 'express';
+import { createConnection } from 'mysql';
+
+const database = createConnection({
+  host: `localhost`,
+  port: 13306,
+  user: `craftsman`,
+  password: `password`,
+  database: `scenario`,
+});
+database.connect();
 
 const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const router = express.Router();
-router.get('/', async (request: express.Request, response: express.Response): Promise<void> => {
-  response.json({
+router.get('/', async (req: express.Request, res: express.Response): Promise<void> => {
+  res.json({
     description: 'Welcome Imaginary store Inventory API.',
     infomation: {
       title: 'Imaginary store Inventory API',
@@ -21,24 +31,26 @@ router.get('/', async (request: express.Request, response: express.Response): Pr
   });
 });
 
-router.get('/v1/stocks/:id', async (request: express.Request, response: express.Response): Promise<void> => {
-  if (Number.parseInt(request.params.id) === 1) {
-    response.json({ id: 1, type: 'supermarket' });
-  } else {
-    response.json({});
-  }
+router.get('/v1/stocks/:id', async (req: express.Request, res: express.Response): Promise<void> => {
+  database.query(`SELECT * from stocks where stocks.id = ${req.params.id};`, function (err, rows) {
+    if (rows.length < 0) {
+      throw err;
+    }
+    res.json({ id: rows[0].id, name: rows[0].name });
+  });
+  database.end();
 });
 
 // TODO FIX
-router.get('/v1/products/:id', async (request: express.Request, response: express.Response): Promise<void> => {
+router.get('/v1/products/:id', async (req: express.Request, res: express.Response): Promise<void> => {
   const samples = [`orange`, `apple`];
   let result;
-  if (Number.parseInt(request.params.id) > -1) {
-    result = samples[Number.parseInt(request.params.id)];
-    response.json(result);
+  if (Number.parseInt(req.params.id) > -1) {
+    result = samples[Number.parseInt(req.params.id)];
+    res.json(result);
   } else {
     result = `No Contents.`;
-    response.json(result);
+    res.json(result);
   }
 });
 
