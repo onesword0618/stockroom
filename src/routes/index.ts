82aad8c6248d3a093e5de267647e9b5168a06a90
onesword0618 <ao.akua.leo@gmail.com>
2022-01-16 23:28:29 +0900

    Fix: importの内容を整理した

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,8 @@
  * Copyright (c) 2022.
  * Kenichi Inoue.
  */
-import express, { Application } from 'express';
-import { createConnection } from 'mysql';
+import express, { Application, Request, Response } from 'express';
+import { createConnection, MysqlError } from 'mysql';
 
 const database = createConnection({
   host: `localhost`,
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const router = express.Router();
-router.get('/', async (req: express.Request, res: express.Response): Promise<void> => {
+app.get('/', async (req: Request, res: Response): Promise<void> => {
   res.json({
     description: 'Welcome Imaginary store Inventory API.',
     infomation: {
@@ -31,14 +31,26 @@ router.get('/', async (req: express.Request, res: express.Response): Promise<voi
   });
 });
 
-router.get('/v1/stocks/:id', async (req: express.Request, res: express.Response): Promise<void> => {
-  database.query(`SELECT * from stocks where stocks.id = ${req.params.id};`, function (err, rows) {
+type Stock = {
+  id: string;
+  name: string;
+  address: string;
+};
+
+router.get('/v1/stocks/:id', async (req: Request, res: Response): Promise<void> => {
+  const stocks: Stock[] = [];
+  database.query(`SELECT * from stocks where stocks.id = ${req.params.id};`, function (err: MysqlError, rows: Stock[]) {
     if (rows.length < 0) {
-      throw err;
+      res.statusCode = 404;
+      res.statusMessage = `Not Found`;
+      database.end();
     }
-    res.json({ id: rows[0].id, name: rows[0].name });
+
+    rows.forEach((element) => {
+      stocks.push(element);
+    });
+    res.json(stocks);
   });
-  database.end();
 });
 
 // TODO FIX
